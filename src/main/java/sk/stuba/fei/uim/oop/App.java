0721cb1de5e914cb245bdf39f48413a533dd545a
AdrianAmenKoSoroka <xsorokaa@stuba.sk>
2022-02-16 23:20:09 +0100

    Updated App.java - Designed to Compare any two numbers

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -3,17 +3,22 @@ package sk.stuba.fei.uim.oop;
 public class App {
     public static void main(String[] args){
         System.out.println("Hello world!");
-        boolean istrue = false;
-        int to = 15;
-        for (int i = 10; i < to; i++){
-            App.Vypis(i,to);
+
+        int[] A_arr = new int[]{1,6,7,9,10,15,20,-5,3,-6};
+        int[] B_arr = new int[]{7,-3,-2,6,-90,90,-2,-5,3};
+
+        for (int index = 0; index < A_arr.length && index < B_arr.length; index++) {
+            System.out.println(App.Compare(A_arr[index], B_arr[index]));
         }
 
     }
 
-    public static void Vypis(int from, int to){
-        for (int i = from; i <= to; i++){
-            System.out.println("Num -> " + i);
+    public static int Compare(int A, int B){
+        if (A > B){
+            return 2;
+        }else if (A == B){
+            return 0;
         }
+        return -1;
     }
 }
