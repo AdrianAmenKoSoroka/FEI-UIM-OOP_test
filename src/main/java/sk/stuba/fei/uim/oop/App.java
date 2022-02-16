08f6e07e38f136b9989436725666c45903d018e6
AdrianAmenKoSoroka <xsorokaa@stuba.sk>
2022-02-16 23:08:25 +0100

    Updated App.java - new function

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -4,12 +4,16 @@ public class App {
     public static void main(String[] args){
         System.out.println("Hello world!");
         boolean istrue = false;
-
-        if (5+4 == 10){
-            istrue = true;
-        } else{
-            istrue = false;
+        int to = 15;
+        for (int i = 10; i < to; i++){
+            App.Vypis(i,to);
         }
-        System.out.println("5+4 == 10 ? -> " + istrue);
+
+    }
+
+    public static void Vypis(int from, int to){
+        for (int i = from; i <= to; i++){
+            System.out.println("Num -> " + i);
+        }
     }
 }
