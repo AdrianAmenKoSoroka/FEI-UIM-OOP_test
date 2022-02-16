92f21fb7329f4c8d74b8f7e79f91690474e045f5
AdrianAmenKoSoroka <xsorokaa@stuba.sk>
2022-02-16 23:03:00 +0100

    Updated App.java

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -1,5 +1,15 @@
 package sk.stuba.fei.uim.oop;
 
 public class App {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        boolean istrue = false;
 
+        if (5+4 == 10){
+            istrue = true;
+        } else{
+            istrue = false;
+        }
+        System.out.println("5+4 == 10 ? -> " + istrue);
+    }
 }
